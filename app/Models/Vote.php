e8006f9af792899167f92c03407049da38cdd2b8
Akosiyuyi <hugojbiscocho@gmail.com>
2026-01-09 16:47:22 +0800

    refac: added HasFactory for test purposes

--- app/Models/Vote.php
+++ app/Models/Vote.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Vote extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'election_id',
         'student_id',
--- app/Models/VoteDetail.php
+++ app/Models/VoteDetail.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class VoteDetail extends Model
 {
+    use HasFactory;
+
     protected $fillable = [
         'vote_id',
         'position_id',
