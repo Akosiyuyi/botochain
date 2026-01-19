d0fa4ebab0ceaaff7eada61add9b3c8536f75a69
Akosiyuyi <hugojbiscocho@gmail.com>
2026-01-20 00:03:03 +0800

    feat: added recent login logic

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\LoginLogs;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Http\RedirectResponse;
 use Illuminate\Http\Request;
@@ -18,9 +19,30 @@ class ProfileController extends Controller
      */
     public function edit(Request $request): Response
     {
+        // Get recent login activity for current user
+        $recentLogins = LoginLogs::where('email', $request->user()->email)
+            ->orderBy('login_attempt_time', 'desc')
+            ->take(10)
+            ->get()
+            ->map(function ($log) {
+                return [
+                    'id' => $log->id,
+                    'date' => $log->login_attempt_time->format('M d, Y'),
+                    'time' => $log->login_attempt_time->format('h:i A'),
+                    'ip_address' => $log->ip_address,
+                    'device' => $this->parseDevice($log->user_agent),
+                    'platform' => $this->parsePlatform($log->user_agent),
+                    'browser' => $this->parseBrowser($log->user_agent),
+                    'status' => $log->status,
+                    'reason' => $log->reason,
+                    'timestamp' => $log->login_attempt_time->diffForHumans(),
+                ];
+            });
+
         return Inertia::render('Profile/Edit', [
             'mustVerifyEmail' => $request->user() instanceof MustVerifyEmail,
             'status' => session('status'),
+            'recentLogins' => $recentLogins,
         ]);
     }
 
@@ -59,5 +81,56 @@ class ProfileController extends Controller
         $request->session()->regenerateToken();
 
         return Redirect::to('/');
+    }
+
+    /**
+     * Parse device from user agent
+     */
+    private function parseDevice($userAgent)
+    {
+        if (preg_match('/mobile/i', $userAgent)) {
+            return 'Mobile';
+        } elseif (preg_match('/tablet/i', $userAgent)) {
+            return 'Tablet';
+        }
+        return 'Desktop';
+    }
+
+    /**
+     * Parse platform from user agent
+     */
+    private function parsePlatform($userAgent)
+    {
+        if (preg_match('/windows/i', $userAgent)) {
+            return 'Windows';
+        } elseif (preg_match('/mac/i', $userAgent)) {
+            return 'macOS';
+        } elseif (preg_match('/linux/i', $userAgent)) {
+            return 'Linux';
+        } elseif (preg_match('/android/i', $userAgent)) {
+            return 'Android';
+        } elseif (preg_match('/iphone|ipad|ipod/i', $userAgent)) {
+            return 'iOS';
+        }
+        return 'Unknown';
+    }
+
+    /**
+     * Parse browser from user agent
+     */
+    private function parseBrowser($userAgent)
+    {
+        if (preg_match('/edg/i', $userAgent)) {
+            return 'Edge';
+        } elseif (preg_match('/chrome/i', $userAgent)) {
+            return 'Chrome';
+        } elseif (preg_match('/firefox/i', $userAgent)) {
+            return 'Firefox';
+        } elseif (preg_match('/safari/i', $userAgent)) {
+            return 'Safari';
+        } elseif (preg_match('/opera|opr/i', $userAgent)) {
+            return 'Opera';
+        }
+        return 'Unknown';
     }
 }
